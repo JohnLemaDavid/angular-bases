21138b5e48a4b64c0b3b7a8b669555fed71fea27
JohnLema <lema.jdavid@gmail.com>
2023-11-21 15:11:43 -0500

    Fin de Seccion 4

---
+++ src/app/heroes/list/list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+export class ListComponent {
+
+  public heroeNames:string[]= ['Spiderman','Ironman','Hulk','Thor','Doctor Strange']
+  public deleteHero?:string;
+
+  removeLastHero():void{
+    this.deleteHero = this.heroeNames.pop();
+
+  }
+
+
+}
